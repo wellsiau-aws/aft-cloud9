6360a4e456ab6b40446f4054eefb2dbadbf05d30
wellsiau-aws <wellsiau@amazon.com>
2022-01-28 21:45:43 -0800

    bash script

---
+++ cloud9.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# install terraform
+echo "Installing Terraform"
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+
+# download terraform file
+#git clone https://github.com/wellsiau-aws/aft-cloud9.git
+#cd aft-cloud9
+
+# get CT home region 
+export TF_VAR_ct_home_region=$1
+
+# Run Terraform
+terraform init
+terraform apply -auto-approve
+
+# Attach role to Cloud9 instance
+export CLOUD9_EC2=`terraform output -raw cloud9_instance_id`
+export CLOUD9_INSTANCE_PROFILE=`terraform output -raw cloud9_instance_profile`
+echo $CLOUD9_EC2
+echo $CLOUD9_INSTANCE_PROFILE
+aws ec2 associate-iam-instance-profile --iam-instance-profile Arn=$CLOUD9_INSTANCE_PROFILE --instance-id $CLOUD9_EC2 --region $TF_VAR_ct_home_region
