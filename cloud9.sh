507b1a3d98acc22e1c9d661ba3b39a74e5a87c02
wellsiau-aws <wellsiau@amazon.com>
2022-01-28 21:47:56 -0800

    fix output

--- cloud9.sh
+++ cloud9.sh
@@ -19,6 +19,9 @@ terraform apply -auto-approve
 # Attach role to Cloud9 instance
 export CLOUD9_EC2=`terraform output -raw cloud9_instance_id`
 export CLOUD9_INSTANCE_PROFILE=`terraform output -raw cloud9_instance_profile`
-echo $CLOUD9_EC2
-echo $CLOUD9_INSTANCE_PROFILE
+#echo $CLOUD9_EC2
+#echo $CLOUD9_INSTANCE_PROFILE
 aws ec2 associate-iam-instance-profile --iam-instance-profile Arn=$CLOUD9_INSTANCE_PROFILE --instance-id $CLOUD9_EC2 --region $TF_VAR_ct_home_region
+
+echo "Cloud9 is ready, use the URL below to access it"
+terraform output -raw cloud9_ide
