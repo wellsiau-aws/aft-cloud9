a2c22260b525a53c137b26c5c8f96c8004f47108
wellsiau-aws <wellsiau@amazon.com>
2022-01-28 21:43:30 -0800

    fix output

--- main.tf
+++ main.tf
@@ -30,6 +30,22 @@ resource "aws_cloud9_environment_ec2" "cloud9-aft" {
   instance_type = "t2.micro"
 }
 
+data "aws_instance" "cloud9-instance" {
+  filter {
+    name = "tag:aws:cloud9:environment"
+    values = [
+    aws_cloud9_environment_ec2.cloud9-aft.id]
+  }
+}
+
 output "cloud9_ide" {
   value = "https://${var.ct_home_region}.console.aws.amazon.com/cloud9/ide/${aws_cloud9_environment_ec2.cloud9-aft.id}?region=${var.ct_home_region}"
+}
+
+output "cloud9_instance_id" {
+  value = data.aws_instance.cloud9-instance.id
+}
+
+output "cloud9_instance_profile" {
+  value = aws_iam_instance_profile.cloud9-aft-profile.arn
 }
